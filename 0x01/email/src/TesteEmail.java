c6edbf8bce565ac8fcaa7dce81e2e24f990378c7
Luis <luis_8.51@hotmail.com>
2022-06-22 11:06:21 -0300

    JUnit

--- 0x01/email/src/TesteEmail.java
+++ 0x01/email/src/TesteEmail.java
@@ -1,17 +1,23 @@
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class TesteEmail {
 
-
+        static Pessoa pessoa;
+        @Test
         public static void testar_email_com_arroba() {
-            assertTrue(Pessoa.emailValid("email_teste@dominio.com.br"));
+            assertTrue(pessoa.emailValid("email_teste@dominio.com.br"));
         }
 
+        @Test
         public static void testar_email_sem_arroba() {
-            assertFalse(Pessoa.emailValid("email_testedominio.com.br"));
+            assertFalse(pessoa.emailValid("email_testedominio.com.br"));
         }
 
-
+        @Test
         public static void testar_email_mais_50_caracteres() {
-            assertEquals(false, Pessoa.emailValid("email_teste_muito_longo_nao_deve_ser_valido@dominio.com.br"));
+            assertEquals(false, pessoa.emailValid("email_teste_muito_longo_nao_deve_ser_valido@dominio.com.br"));
         }
 
 }
